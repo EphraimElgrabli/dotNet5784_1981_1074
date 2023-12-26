c9ad774b94ee7aef494fe38410d4a5de89edee32
EphraimElgrabli <ephraim.elgrabli@gmail.com>
2023-12-26 23:36:43 +0200

    Initial code + welcome functions

---
+++ Stage0/Program1074.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1074();
+            Welcome1981();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome1074();
+        private static void Welcome1981() { }
+
+    }
+}
