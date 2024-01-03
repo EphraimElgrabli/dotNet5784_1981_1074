49154c6b8f856167088acab4e28b45888de6ffa6
EphraimElgrabli <ephraim.elgrabli@gmail.com>
2024-01-03 23:35:47 +0200

    working on the DAta source

---
+++ DalList/DataSource.cs
@@ -0,0 +1,13 @@
+﻿namespace Dal;
+
+internal static class DataSource
+{
+    internal static class Config
+    {
+        internal
+    }
+    internal static List<DO.Config> Configs { get; } = new();
+    internal static List<DO.Dependency> Dependencys { get; } = new();
+    internal static List<DO.Engineer> Engineers { get; } = new();
+    internal static List<DO.Task> Tasks { get; } = new();
+}
