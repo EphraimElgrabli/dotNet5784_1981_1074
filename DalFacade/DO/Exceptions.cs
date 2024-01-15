fb8f9eb0bd84ffadf8fcecae23af9e5f5d9f55d7
EphraimElgrabli <ephraim.elgrabli@gmail.com>
2024-01-15 16:55:49 +0200

    adding comments to the DO class.

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,15 +1,27 @@
 ﻿namespace DO;
 
+/// <summary>
+/// Exception thrown when a Data Access Layer (DAL) does not exist.
+/// </summary>
 [Serializable]
 public class DalDoesNotExistException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DalDoesNotExistException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public DalDoesNotExistException(string? message) : base(message) { }
 }
 
+/// <summary>
+/// Exception thrown when a Data Access Layer (DAL) already exists.
+/// </summary>
 [Serializable]
 public class DalAlreadyExistException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DalAlreadyExistException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public DalAlreadyExistException(string? message) : base(message) { }
 }
-
-
