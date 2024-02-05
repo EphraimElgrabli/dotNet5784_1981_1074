462e90086ba4c01d85f94cb0313534e98ec4f21d
EphraimElgrabli <ephraim.elgrabli@gmail.com>
2024-02-05 23:24:11 +0200

    added an interface for milestone, im not sure we will implemt it.

---
+++ BL/BlApi/IMilestone.cs
@@ -0,0 +1,15 @@
+﻿using BO;
+
+namespace BlApi;
+
+public interface IMilestone
+{
+    public int Create(Milestone mstone);
+    public void Delete(int id);
+    public void Update(Milestone mstone);
+    public IEnumerable<MilestoneInList> ReadAll();
+    public Milestone Read(int id);
+}
+//not sure i will implement it, look complicated AF,
+//i coudenlt understand by myself what does milestone in list mean,
+//wasn't written in the manuel
