660f866b00df47128203948742bc4261d5697457
EphraimElgrabli <ephraim.elgrabli@gmail.com>
2024-02-12 20:27:33 +0200

    Finished up to chapter 5 in stage 5. ive created a main menu and user list menu.

---
+++ PL/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using iNKORE.UI.WPF.Modern.Controls.Helpers;
+using iNKORE.UI.WPF.Modern.Controls.Primitives;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
