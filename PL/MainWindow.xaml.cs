e738d3c7da44dbd0612a560ca03cfeda843b00c2
EphraimElgrabli <ephraim.elgrabli@gmail.com>
2024-02-16 13:58:06 +0200

    I made Some Bugs purposfully

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,14 +1,15 @@
 ﻿using PL.User;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.MessageBox;
+using MessageBoxButton = System.Windows.MessageBoxButton;
+using MessageBoxResult = System.Windows.MessageBoxResult;
 
 namespace PL
 {
@@ -29,10 +30,11 @@ namespace PL
 
         private void initDb_Click(object sender, RoutedEventArgs e)
         {
-            if (MessageBox.Show("Are you sure you to initialize the Database?","InitDB - Warning.", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            if (MessageBox.Show("Are you sure you to initialize the Database?", "InitDB - Warning.", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
             {
                     DalTest.Initialization.Do();
             }
         }
     }
+    //(MessageBox.Show("Are you sure you to initialize the Database?","InitDB - Warning.", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
 }
