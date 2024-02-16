11a714f264cf54d774f553e1eef1854831e81f09
EphraimElgrabli <ephraim.elgrabli@gmail.com>
2024-02-16 14:08:44 +0200

    fixing all my bugs

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using PL.User;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MessageBox = System.Windows.MessageBox;
-using MessageBoxButton = System.Windows.MessageBoxButton;
-using MessageBoxResult = System.Windows.MessageBoxResult;
 
 namespace PL
 {
@@ -32,9 +31,8 @@ namespace PL
         {
             if (MessageBox.Show("Are you sure you to initialize the Database?", "InitDB - Warning.", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
             {
-                    DalTest.Initialization.Do();
+                DalTest.Initialization.Do();
             }
         }
     }
-    //(MessageBox.Show("Are you sure you to initialize the Database?","InitDB - Warning.", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
 }
